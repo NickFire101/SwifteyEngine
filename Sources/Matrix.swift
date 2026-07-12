e80a97a9672122c40ebd44ca78f4310680ed2ac5
NickFire101 <ODCADeveloper@outlook.com>
2026-07-12 10:15:51 +0800

    Create Matrix.swift

---
+++ Sources/Matrix.swift
@@ -0,0 +1,22 @@
+public struct Matrix: Sendable {
+
+    public let rows: Int
+
+    public let columns: Int
+
+    public var values: [Float]
+
+    public init(
+        rows: Int,
+        columns: Int,
+        values: [Float]
+    ) {
+
+        precondition(values.count == rows * columns)
+
+        self.rows = rows
+        self.columns = columns
+        self.values = values
+    }
+
+}
